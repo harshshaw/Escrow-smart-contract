46112b3d17fd1c2f7243c167b603ad84abcaaa3b
Remix Workspace <>
2022-08-31 00:15:33 +0530

    added buyer and seller

---
+++ contracts/Escrow.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.7.0;
+
+
+interface IERC721{
+    function safeTransferFrom(address _from,address _to,uint _id) external;
+}
+
+contract Escrow   {
+    //Transfer ownership of property
+    address public nftAddress;
+    uint public nftId;
+    address payable seller;
+    address payable buyer;
+
+    constructor (
+        address _nftAddress,
+        uint256 _nftId,
+        address payable _seller,
+        address payable _buyer
+    )  {
+        nftAddress=_nftAddress;
+        nftId=_nftId;
+        seller=_seller;
+        buyer=_buyer;
+    }
+
+    function finalizeSale() public {
+        IERC721(nftAddress).safeTransferFrom(seller,buyer,nftId);
+    }
+
+    
+}
