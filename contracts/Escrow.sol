df84781179ff605dbd8903ea400e5bb230de5879
Remix Workspace <>
2022-08-31 22:08:00 +0530

    added a few more test console.log

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -11,21 +11,47 @@ contract Escrow   {
     //Transfer ownership of property
     address public nftAddress;
     uint public nftId;
+    uint256 public purchasePrice;
+    uint256 public escrowAmount;
     address payable seller;
     address payable buyer;
+    address public inspector;
+    address public lender;
 
     constructor (
         address _nftAddress,
         uint256 _nftId,
+        uint256 _purchasePrice,
+        uint256 _escrowAmount,
         address payable _seller,
-        address payable _buyer
+        address payable _buyer,
+        address  _inspector,
+        address  _lender
     )  {
         nftAddress=_nftAddress;
         nftId=_nftId;
         seller=_seller;
         buyer=_buyer;
+        purchasePrice=_purchasePrice;
+        escrowAmount=_escrowAmount;
+        inspector=_inspector;
+        lender=_lender;
     }
 
+    modifier onlyBuyer(){
+         require(msg.sender==buyer,"Only buyer is allowed to send earnest");
+        _;
+    }
+    function depositEarnest() public payable onlyBuyer{
+        require(msg.value>=20 ether,"Minimum amount not paid");
+
+    }
+
+    function getBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
+    //A function to Finalize Sale and transfer nft from sellers address to buyer.
     function finalizeSale() public {
         IERC721(nftAddress).safeTransferFrom(seller,buyer,nftId);
     }
--- scripts/Escrow.test.js
+++ scripts/Escrow.test.js
@@ -1,15 +1,26 @@
 const {expect}=require('chai');
 const {ethers}=require('hardhat');
 
+//Function to convert ether to wei
+const tokens=(n)=>{
+    return ethers.utils.parseUnits(n.toString(),'ether');
+}
+
+const ether=tokens;
+
 describe("RealEstate",()=>{
     let realEstate,escrow;
-    let deployer,seller,buyer;
+    let deployer,seller,buyer,inspector,lender; 
     let nftID=1;
+    let purchasePrice=ether(100);
+    let escrowAmount=ether(20);
     beforeEach(async()=>{
         //Setup Accounts
         accounts=await ethers.getSigners();
         deployer=accounts[0];
         buyer=accounts[1];
+        inspector=accounts[2];
+        lender=accounts[3];
         seller=deployer;
         //Load Contracts
         const RealEstate=await ethers.getContractFactory('RealEstate');
@@ -20,8 +31,12 @@ describe("RealEstate",()=>{
         escrow=await Escrow.deploy(
             realEstate.address,
             nftID,
+            purchasePrice,
+            escrowAmount //20% downpayment for Escrow
             seller.address,
             buyer.address
+            inspector.address,
+            lender.address,
         );
 
         //Seller Approves NFT
@@ -36,14 +51,27 @@ describe("RealEstate",()=>{
     })
 
     describe('Selling real estate',async()=>{
+        let balance,transaction;
         it('executes a successful transaction ' async()=>{
             //Expect seller to be the NFT owner before the sale
             expect(await realEstate.ownerOf(nftID)).to.equal(seller.address)
+            
+            //Check escrow balance
+            balance=await escrow.getBalance();
+            // console.log("escrow balance:",ethers.utils.formatEther(balance))
+
+            //Buyer Deposit to Earnest
+            transaction=await escrow.connect(buyer).depositEarnest({value:escrowAmount});
+            // console.log("buyer deposits earnest money");
+            
+            //Check escrow balance
+            balance=await escrow.getBalance();
+            // console.log("escrow balance:",ethers.utils.formatEther(balance))
 
             //finalize sale
             transaction=await escrow.connect(buyer).finalizeSale()
             await transaction.wait()
-            console.log("buyer finalize sale")
+            // console.log("buyer finalize sale")
 
             //Expect the buyer to be the NFT owner after the sale
             expect(await realEstate.ownerOf(nftID)).to.equal(buyer.address)
